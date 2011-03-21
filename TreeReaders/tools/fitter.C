8e3eba7e3d9d0568a1571b0b427e5503d2752cac
nancymarinelli <>
2011-03-21 15:40:29 +0000

    This commit was manufactured by cvs2svn to create tag 'NM_21March2011'.
    Sprout from master 2011-03-21 15:40:28 UTC nancy 'Access the branches gp_pdgid and gp_status'
    Cherrypick from master 2010-11-23 18:47:00 UTC nancy 'Restrict the range for plotting the final plot':
        TreeReaders/tools/fitter.C

--- TreeReaders/tools/fitter.C
+++ TreeReaders/tools/fitter.C
@@ -1,4 +1,4 @@
-void fitter (char * filename = "/data/ndpc2/c/HiggsGammaGamma/CMSSW_3_8_5_patch3/src/ND_Hto2Photons/TreeReaders/UnweightedHiggsAnalysis130GeV.root", float startmass = 130) {
+void fitter (char * filename = "/data/ndpc2/c/HiggsGammaGamma/CMSSW_3_8_5_patch3/src/ND_Hto2Photons/TreeReaders/HiggsAnalysis120GeVunweighted.root", float startmass = 120) {
   using namespace RooFit;
 
   //  gStyle->SetOptStat(0);
@@ -6,7 +6,7 @@ void fitter (char * filename = "/data/ndpc2/c/HiggsGammaGamma/CMSSW_3_8_5_patch3
   gROOT->SetStyle("Plain");
   gStyle->SetPadLeftMargin(0.15);
   gStyle->SetTitleOffset(1.8,"y");
-  TCanvas * c1 = new TCanvas("c1", "c1",430, 10, 800, 800);
+  TCanvas * c1 = new TCanvas("c1", "c1",430, 10, 600, 600);
   c1->SetBorderMode(0);
   
   TFile * hFile = new TFile(filename);
@@ -15,12 +15,12 @@ void fitter (char * filename = "/data/ndpc2/c/HiggsGammaGamma/CMSSW_3_8_5_patch3
   for (Int_t i=1; i<HistList->GetSize(); ++i) {
     TString HistName(HistList->At(i)->GetName());
     //cout << "Looking at Hist: " << HistName << endl; 
-    if (HistName.Contains("mass_2gamma1goodconv")==1) {
+    if (HistName.Contains("mass")==1) {
       // Parameter used for mass in both CrystalBall or BreitWigner
       RooRealVar mass("mass","Mass_{2#gamma}", 80, 160,"GeV/c^{2}");
 
       //  Parameters for Crystal Ball Lineshape 
-      RooRealVar m0("M_{Higgs}", "Bias", startmass, startmass-5, startmass+5);//,"GeV/c^{2}"); 
+      RooRealVar m0("Mass Higgs_{2#gamma}", "Bias", startmass, startmass-5, startmass+5);//,"GeV/c^{2}"); 
       RooRealVar sigma("#sigma_{CB}","Width", 2,0,10.0);//,"GeV/c^{2}"); 
       RooRealVar cut("#alpha","Cut", 2,0,20.0); 
       RooRealVar power("#gamma","Power", 2, 0, 20.0); 
@@ -40,18 +40,16 @@ void fitter (char * filename = "/data/ndpc2/c/HiggsGammaGamma/CMSSW_3_8_5_patch3
       TString DataHistName = HistName;
       DataHistName += "fit";
       RooDataHist data(DataHistName,DataHistName,mass,DataHist,1.0);
-      CrystalBall.fitTo(data,Range(startmass-10, startmass+20));
+      CrystalBall.fitTo(data,Range(startmass-20, startmass+10));
       RooPlot * plot = mass.frame(80,160,80);
       data.plotOn(plot,DataError(RooAbsData::None));
-      plot->SetMaximum(1.2*plot->GetMaximum());
       CrystalBall.plotOn(plot);
       //CrystalBall.paramOn(plot);
-      CrystalBall.paramOn(plot,Format("NEL",FixedPrecision(4)), Parameters(RooArgSet(m0, sigma, cut, power)), Layout(.55, 0.99, 0.99), ShowConstants(kFALSE));
+      CrystalBall.paramOn(plot,Format("NEL",AutoPrecision(3)), Parameters(RooArgSet(m0, sigma, cut, power)), Layout(.60, 0.99, 0.99), ShowConstants(kFALSE));
       TString PlotTitle(DataHist->GetTitle());
       PlotTitle += ";Mass_{2#gamma} (GeV/c^{2});Number of Weighted Events";
       plot->SetTitle(PlotTitle);
-      plot->GetXaxis()->SetRangeUser(startmass-20,startmass+10);
-      gStyle->SetTitleW(.54);
+      plot->GetXaxis()->SetRangeUser(100.,135.);
       plot->Draw();
       TString OutPutName = HistName + "fit.gif";
       c1->SaveAs(OutPutName);
