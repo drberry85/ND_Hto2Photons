15d4840a3bd9287251a376f7651f01c251a033a4
drberry <>
2011-03-24 14:18:26 +0000

    Fixed problems with the text

--- TreeReaders/tools/fitter.C
+++ TreeReaders/tools/fitter.C
@@ -6,6 +6,7 @@ void fitter (char * filename = "/data/ndpc2/c/HiggsGammaGamma/CMSSW_3_8_5_patch3
   gROOT->SetStyle("Plain");
   gStyle->SetPadLeftMargin(0.15);
   gStyle->SetTitleOffset(1.8,"y");
+  gStyle->SetTitleW(.54);
   TCanvas * c1 = new TCanvas("c1", "c1",430, 10, 800, 800);
   c1->SetBorderMode(0);
   
@@ -15,7 +16,7 @@ void fitter (char * filename = "/data/ndpc2/c/HiggsGammaGamma/CMSSW_3_8_5_patch3
   for (Int_t i=1; i<HistList->GetSize(); ++i) {
     TString HistName(HistList->At(i)->GetName());
     //cout << "Looking at Hist: " << HistName << endl; 
-    if (HistName.Contains("mass_2gammaGoldenSel")==1) {
+    if (HistName.Contains("mass_")==1) {
       // Parameter used for mass in both CrystalBall or BreitWigner
       RooRealVar mass("mass","Mass_{2#gamma}", 80, 160,"GeV/c^{2}");
 
@@ -27,7 +28,7 @@ void fitter (char * filename = "/data/ndpc2/c/HiggsGammaGamma/CMSSW_3_8_5_patch3
       RooCBShape CrystalBall("CrystalBall", "A  Crystal Ball Lineshape", mass, m0,sigma, cut, power);
             
       //  Parameters for Breit-Wigner Distribution
-      //RooRealVar mRes("M_{Higgs}", "Higgs Resonance  Mass", startmass, 70, 200);//,"GeV/c^{2}"); 
+      //RooRealVar mRes("M_{Higgs}", "Higgs Resonance  Mass", startmass, 80, 160);//,"GeV/c^{2}"); 
       //RooRealVar Gamma("#Gamma", "#Gamma", 1, 0,10.0);//,"GeV/c^{2}"); 
       //RooBreitWigner BreitWigner("BreitWigner","A Breit-Wigner Distribution",mass,mRes,Gamma);
 
@@ -43,7 +44,7 @@ void fitter (char * filename = "/data/ndpc2/c/HiggsGammaGamma/CMSSW_3_8_5_patch3
       RooRealVar fracG("f_{G}", "Gaussian Fraction", 0.0,0.0,1.0);
       RooAddPdf ResolutionModel("ResolutionModel", "Resolution Model", GaussianResolution, CrystalBall, fracG); 
       //fracG.setConstant();
-      //RooFFTConvPdf ResolutionModel("ResolutionModel","Resolution Model", mass, CrystalBall, GaussianResolution);
+      //RooFFTConvPdf ResolutionModel("ResolutionModel","Resolution Model", mass, BreitWigner, CrystalBall);
       
       cout << HistName << " is being fit." << endl;
       TH1F * DataHist = (TH1F*) hFile->Get(HistName);
@@ -55,16 +56,17 @@ void fitter (char * filename = "/data/ndpc2/c/HiggsGammaGamma/CMSSW_3_8_5_patch3
       ResolutionModel.fitTo(data,Range(startmass-20, startmass+10));
       RooPlot * plot = mass.frame(80,160,80);
       data.plotOn(plot,DataError(RooAbsData::None));
-      plot->SetMaximum(1.1*plot->GetMaximum());
+      plot->SetMaximum(1.2*plot->GetMaximum());
       //CrystalBall.plotOn(plot);
       //CrystalBall.paramOn(plot,Format("NEL",FixedPrecision(4)), Parameters(RooArgSet(m0, sigma, cut, power)), Layout(.55, 0.99, 0.99), ShowConstants(kFALSE));
       ResolutionModel.plotOn(plot);
-      ResolutionModel.paramOn(plot,Format("NEL",FixedPrecision(4)), Parameters(RooArgSet(m0, sigma, cut, power, GaussianSigma)), Layout(.15, 0.58, 0.9), ShowConstants(kFALSE));
+      ResolutionModel.paramOn(plot,Format("NEL",FixedPrecision(4)), Parameters(RooArgSet(m0, sigma, cut, power, GaussianSigma, fracG)), Layout(.15, 0.5, 0.9), ShowConstants(kFALSE));
       TString PlotTitle(DataHist->GetTitle());
       PlotTitle += ";Mass_{2#gamma} (GeV/c^{2});Number of Weighted Events";
       plot->SetTitle(PlotTitle);
       plot->GetXaxis()->SetRangeUser(startmass-20,startmass+10);
-      gStyle->SetTitleW(.54);
+      TPaveText *box = (TPaveText*) plot->findObject("ResolutionModel_paramBox");
+      box->SetTextSize(0.03);
       plot->Draw();
       TString OutPutName = HistName + "fit.gif";
       c1->SaveAs(OutPutName);
