faf991356e62a54f8f9f331ea7027829fd6a8e5b
drberry <>
2012-04-30 12:56:08 +0000

    Added Ratio Plots

--- TreeReaders/tools/ZMuMuMVA_stackploter.py
+++ TreeReaders/tools/ZMuMuMVA_stackploter.py
@@ -14,10 +14,10 @@ leg.SetFillColor(0)
 leg.SetBorderSize(1)
 HistogramNames=["pho_idmva","pho_tmva_id_mit_ecal","pho_tmva_id_mit_etawidth","pho_tmva_id_mit_hcal","pho_tmva_id_mit_hoe","pho_tmva_id_mit_nvtx","pho_tmva_id_mit_phiwidth","pho_tmva_id_mit_preshower","pho_tmva_id_mit_r9","pho_tmva_id_mit_sceta","pho_tmva_id_mit_sieie","pho_tmva_id_mit_tiso1","pho_tmva_id_mit_tiso2","pho_tmva_id_mit_tiso3"]
 Categories=["Barrel","Endcap"]
-FileNames=["ZMuMu_Fall11_NoCuts.root","ZMuMu_TTJets_NoCuts.root"]
+FileNames=["ZMuMu_Fall11.root","ZMuMu_TTJets.root"]
 Legends = ["Z#mu#mu#gamma","TT+Jets"]
 pwd = "/data/ndpc2/c/HiggsGammaGamma/PhotonPlusJet/CMSSW_4_2_3/src/ND_Hto2Photons/TreeReaders/"
-DataFile = TFile(pwd+"ZMuMu_Data_NoCuts.root")
+DataFile = TFile(pwd+"ZMuMu_Data.root")
 
 MCFiles=[]
 for file in FileNames:
@@ -29,9 +29,12 @@ for histbase in HistogramNames:
 		Data=DataFile.Get(hist)
 		stack = THStack(hist,"")
 		mchistlist=[]
+		MCHistSum = TH1F()
 		for i in range(len(MCFiles)):
 			MCFiles[i].cd()
 			mchistlist.append(MCFiles[i].Get(hist))
+			if i==0: MCHistSum=mchistlist[i]
+			else: MCHistSum.Add(mchistlist[i])
 			mchistlist[i].SetTitle("")
 			if i==0: stack.SetTitle(hist+";"+mchistlist[0].GetXaxis().GetTitle()+";")
 			mchistlist[i].SetLineWidth(0)
@@ -47,6 +50,13 @@ for histbase in HistogramNames:
 		Data.Draw("esame")
 		leg.Draw()
 		can.SaveAs("ZMuMuMVAStack_"+hist+".png")
+		RatioHist = Data.Clone("")
+		#import pdb; pdb.set_trace()
+		RatioHist.Divide(Data,MCHistSum,1,1,"B")
+		can.SetLogy(0)
+		RatioHist.Draw("")
+		can.SaveAs("ZMuMuMVAStack_"+hist+"_Ratio.png")
+		can.SetLogy()
 		stack.Clear()
 		leg.Clear()
 		gDirectory.Delete("*")
